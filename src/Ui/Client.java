a5b56cd38c348d2d1898662cc3d369e371747b3d
abdeljabaraitayoub <abdeljabarayoubi@GMail.com>
2024-09-17 12:51:35 +0100

    i costumize the client menu

---
+++ src/Ui/Client.java
@@ -0,0 +1,19 @@
+package Ui;
+
+public class Client extends Main {
+
+
+    public void display() {
+        System.out.println("=== Menu Principal ===");
+        System.out.println("1. Create a project");
+        System.out.println("2. List all projects");
+        System.out.println("3. Get project by id");
+        System.out.println("Enter your choice: ");
+
+
+    }
+
+    public void choice() {
+
+    }
+}
